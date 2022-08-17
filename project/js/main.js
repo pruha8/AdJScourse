1cf2fd5e0308c8afe7680e3665946bb39d505639
pruha8 <su2880@mail.ru>
2022-08-17 18:55:26 +0300

    update

--- project/js/main.js
+++ project/js/main.js
@@ -1,22 +1,23 @@
 const products = [
-    {id: 1, title: 'Notebook', price: 2000},
-    {id: 2, title: 'Mouse', price: 20},
-    {id: 3, title: 'Keyboard', price: 200},
-    {id: 4, title: 'Gamepad', price: 50},
+    {id: 1, title: 'Notebook', price: 2000, image: 'img/cart.jpg'},
+    {id: 2, title: 'Mouse', price: 20,image: 'img/cart.jpg'},
+    {id: 3, title: 'Keyboard', price: 200, image: 'img/cart.jpg'},
+    {id: 4, title: 'Gamepad', price: 50, image: 'img/cart.jpg'},
 ];
 //Функция для формирования верстки каждого товара
 //Добавить в выводе изображение
-const renderProduct = (title, price) => {
+const renderProduct = (item) => {
     return `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
+                <h3>${item.title}</h3>
+                <img src="${item.image}" alt="">
+                <p>${item.price}</p>
                 <button class="buy-btn">Купить</button>
             </div>`
 };
 const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item.title,item.price));
+    const productsList = list.map(item => renderProduct(item));
     console.log(productsList);
-    document.querySelector('.products').innerHTML = productsList;
+    document.querySelector('.products').innerHTML = productsList.join('');
 };
 
 renderPage(products);
